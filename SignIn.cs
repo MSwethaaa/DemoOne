12aa0944068290f4341045081546122195d6082b
unknown <Swetha>
2024-02-15 12:35:59 +0530

    Final Commit

---
+++ SignIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class SignIn
+    {
+        public string Autentication(string username, string password)
+        {
+            string msg;
+            if((string.IsNullOrEmpty(username)) || string.IsNullOrEmpty(password))
+            {
+                msg = "User name and password required";
+            }
+            else
+            {
+                if((username=="admin") &&(password =="admin@1234"))
+                {
+                    msg = "Autentication Pass";
+                }
+                else
+                {
+                    msg = "Autentication Fail";
+                }
+                
+            }
+            return msg;
+        }
+    }
+}
